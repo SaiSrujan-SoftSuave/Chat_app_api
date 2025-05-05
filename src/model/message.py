6ee1065438b382b20bf8ef0fbbefb41231766151
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2025-05-05 17:25:12 +0530

    updated the message model

--- src/model/message.py
+++ src/model/message.py
@@ -1,13 +1,16 @@
 import uuid
 from datetime import datetime
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
+from pydantic import BaseModel
 from sqlmodel import SQLModel, Field, Relationship
 
 # if TYPE_CHECKING:
-from src.model.user import User
+# from src.model.user import User
 from src.model.user import UserRead
 
+if TYPE_CHECKING:
+    from .message import Message
 
 
 # from src.model import User
@@ -17,6 +20,8 @@ from src.model.user import UserRead
 class MessageBase(SQLModel):
     content: str
     timestamp: datetime = Field(default_factory=datetime.now)
+    seen_timestamp: Optional[datetime] = Field(default=None, nullable=True)
+    is_deleted: bool = Field(default=False, nullable=False)
 
 
 class Message(MessageBase, table=True):
@@ -25,12 +30,22 @@ class Message(MessageBase, table=True):
     sender_id: uuid.UUID = Field(foreign_key="user.id")
     receiver_id: uuid.UUID = Field(foreign_key="user.id")
 
-    # Relationships
-    sender: User = Relationship(back_populates="sent_messages")
-    receiver: User = Relationship(back_populates="received_messages")
+    sender: "User" = Relationship(
+        back_populates="sent_messages",
+        sa_relationship_kwargs={"foreign_keys": "[Message.sender_id]"}
+    )
+    receiver: "User" = Relationship(
+        back_populates="received_messages",
+        sa_relationship_kwargs={"foreign_keys": "[Message.receiver_id]"}
+    )
 
 
 class MessageRead(MessageBase):
     id: uuid.UUID
     sender: UserRead
     receiver: UserRead
+
+
+class MesageUpdate(BaseModel):
+    id: uuid.UUID
+    new_message: str
